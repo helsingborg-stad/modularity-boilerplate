d382c7ec38a4022c6cbf1579e41a117507974145
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-16 14:08:45 +0100

    Enqueue module script

--- source/php/Module/Boilerplate.php
+++ source/php/Module/Boilerplate.php
@@ -72,6 +72,24 @@ class {{BPREPLACESLUGCAMELCASE}} extends \Modularity\Module
     }
 
     /**
+     * Script - Register & adding scripts
+     * @return void
+     */
+    public function script()
+    {
+        //Register custom css
+        wp_register_script(
+            'modularity-{{BPREPLACESLUG}}',
+            MODULARITYRECOMMEND_URL . '/dist/' . CacheBust::name('css/modularity-{{BPREPLACESLUG}}.css'),
+            null,
+            '1.0.0'
+        );
+
+        //Enqueue
+        wp_enqueue_script('modularity-{{BPREPLACESLUG}}');
+    }
+
+    /**
      * Available "magic" methods for modules:
      * init()            What to do on initialization
      * data()            Use to send data to view (return array)
