0e8691fe8cb0ece399f66364cb42b24ba5eaa8ad
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2022-02-16 13:42:52 +0100

    Add replacement keys to main file

--- modularity-boilerplate.php
+++ modularity-boilerplate.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * Plugin Name:       Modularity Recommend
- * Plugin URI:        https://github.com/helsingborg-stad/modularity-recommend
- * Description:       Display Recommended links, static or AI generated links from RekAI.
+ * Plugin Name:       {{BPREPLACENAME}}
+ * Plugin URI:        https://github.com/{{BPREPLACEGITHUB}}/modularity-{{BPREPLACESLUG}}
+ * Description:       {{BPREPLACEDESCRIPTION}}
  * Version:           1.0.0
- * Author:            Sebastian Thulin
- * Author URI:        https://github.com/sebastianthulin
+ * Author:            {{BPREPLACEAUTHOR}}
+ * Author URI:        https://github.com/{{BPREPLACEGITHUB}}
  * License:           MIT
  * License URI:       https://opensource.org/licenses/MIT
- * Text Domain:       mod-recommend
+ * Text Domain:       mod-{{BPREPLACESLUG}}
  * Domain Path:       /languages
  */
 
@@ -18,39 +18,38 @@ if (! defined('WPINC')) {
     die;
 }
 
-define('MODULARITYRECOMMEND_PATH', plugin_dir_path(__FILE__));
-define('MODULARITYRECOMMEND_URL', plugins_url('', __FILE__));
-define('MODULARITYRECOMMEND_TEMPLATE_PATH', MODULARITYRECOMMEND_PATH . 'templates/');
-define('MODULARITYRECOMMEND_VIEW_PATH', MODULARITYRECOMMEND_PATH . 'views/');
-define('MODULARITYRECOMMEND_MODULE_VIEW_PATH', plugin_dir_path(__FILE__) . 'source/php/Module/views');
-define('MODULARITYRECOMMEND_MODULE_PATH', MODULARITYRECOMMEND_PATH . 'source/php/Module/');
+define('{{BPREPLACECAPSCONSTANT}}_PATH', plugin_dir_path(__FILE__));
+define('{{BPREPLACECAPSCONSTANT}}_URL', plugins_url('', __FILE__));
+define('{{BPREPLACECAPSCONSTANT}}_TEMPLATE_PATH', {{BPREPLACECAPSCONSTANT}}_PATH . 'templates/');
+define('{{BPREPLACECAPSCONSTANT}}_VIEW_PATH', {{BPREPLACECAPSCONSTANT}}_PATH . 'views/');
+define('{{BPREPLACECAPSCONSTANT}}_MODULE_VIEW_PATH', plugin_dir_path(__FILE__) . 'source/php/Module/views');
+define('{{BPREPLACECAPSCONSTANT}}_MODULE_PATH', {{BPREPLACECAPSCONSTANT}}_PATH . 'source/php/Module/');
 
-load_plugin_textdomain('modularity-recommend', false, plugin_basename(dirname(__FILE__)) . '/languages');
+load_plugin_textdomain('modularity-{{BPREPLACESLUG}}', false, plugin_basename(dirname(__FILE__)) . '/languages');
 
-require_once MODULARITYRECOMMEND_PATH . 'source/php/Vendor/Psr4ClassLoader.php';
-require_once MODULARITYRECOMMEND_PATH . 'Public.php';
+require_once {{BPREPLACECAPSCONSTANT}}_PATH . 'source/php/Vendor/Psr4ClassLoader.php';
+require_once {{BPREPLACECAPSCONSTANT}}_PATH . 'Public.php';
 
 // Instantiate and register the autoloader
-$loader = new ModularityRecommend\Vendor\Psr4ClassLoader();
-$loader->addPrefix('ModularityRecommend', MODULARITYRECOMMEND_PATH);
-$loader->addPrefix('ModularityRecommend', MODULARITYRECOMMEND_PATH . 'source/php/');
+$loader = new {{BPREPLACENAMESPACE}}\Vendor\Psr4ClassLoader();
+$loader->addPrefix('{{BPREPLACENAMESPACE}}', {{BPREPLACECAPSCONSTANT}}_PATH);
+$loader->addPrefix('{{BPREPLACENAMESPACE}}', {{BPREPLACECAPSCONSTANT}}_PATH . 'source/php/');
 $loader->register();
 
 // Acf auto import and export
 $acfExportManager = new \AcfExportManager\AcfExportManager();
 $acfExportManager->setTextdomain('modularity-recommend');
-$acfExportManager->setExportFolder(MODULARITYRECOMMEND_PATH . 'source/php/AcfFields/');
+$acfExportManager->setExportFolder({{BPREPLACECAPSCONSTANT}}_PATH . 'source/php/AcfFields/');
 $acfExportManager->autoExport(array(
-    'recommended' => 'group_61ea7a87e8e9f',
-    'settings' => 'group_61eaa5feb2601',
+    '{{BPREPLACESLUG}}' => 'group_61ea7a87e8e9f' //Update with acf id here
 ));
 $acfExportManager->import();
 
 // Modularity 3.0 ready - ViewPath for Component library
 add_filter('/Modularity/externalViewPath', function ($arr) {
-    $arr['mod-recommend'] = MODULARITYRECOMMEND_MODULE_VIEW_PATH;
+    $arr['mod-{{BPREPLACESLUG}}'] = {{BPREPLACECAPSCONSTANT}}_MODULE_VIEW_PATH;
     return $arr;
 }, 10, 3);
 
 // Start application
-new ModularityRecommend\App();
+new {{BPREPLACENAMESPACE}}\App();
